4db0c2b1d71ef507df96728a00acb0461c76e722
King-AJr <talk2king.aj@gmail.com>
2023-07-26 23:24:30 +0000

    run multiple promises together using promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,8 @@
-import uploadPhoto,createUser from "../utils.js";
+/* eslint-disable no-console */
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignUp() {
+	return Promise.all([uploadPhoto(), createUser()])
+	.then(message => console.log(message[1].firstName + " " + message[1].lastName))
+	.catch(() => new Error ('SignUp system offline'))
+}
