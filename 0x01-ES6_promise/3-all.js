51c19f1a4c4c58874cb6e6ec819d19737df83231
King-AJr <talk2king.aj@gmail.com>
2023-07-28 18:17:24 +0000

    rejecting in promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignUp() {
 	return Promise.all([uploadPhoto(), createUser()])
-	.then(message => console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`))
-	.catch(() => {console.log('SignUp system offline');})
+	.then(message => { console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`); });
+	.catch(() => { console.log('SignUp system offline'); });
 }
