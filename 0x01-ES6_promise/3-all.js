244178a6e23a2d744f21542be0017dbea6f1b548
King-AJr <talk2king.aj@gmail.com>
2023-07-28 17:42:55 +0000

    console.log on reject

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ import { uploadPhoto, createUser } from "./utils";
 export default function handleProfileSignUp() {
 	return Promise.all([uploadPhoto(), createUser()])
 	.then(message => console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`))
-	.catch(() => new Error ('SignUp system offline'))
+	.catch(() => {console.log('SignUp system offline');})
 }
