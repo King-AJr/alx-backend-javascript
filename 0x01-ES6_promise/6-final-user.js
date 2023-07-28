f172e40b92ba025508ed938b284500d28031c61c
King-AJr <talk2king.aj@gmail.com>
2023-07-28 18:55:27 +0000

    output array of results

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,6 @@
 import SignUpUser from "./4-user-promise"
 import uploadPhoto from "./5-photo-reject"
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.allSettled([SignUpUser(firstName, lastName), uploadPhoto(fileName)]);
+	return Promise.allSettled([SignUpUser(firstName, lastName), uploadPhoto(fileName)])
+	.then((res) => console.log(res));
 }
