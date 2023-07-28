8b7683b39356f9481571477fb6d0a70747e11add
King-AJr <talk2king.aj@gmail.com>
2023-07-28 19:32:27 +0000

    output array of results

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,14 @@ import SignUpUser from "./4-user-promise"
 import uploadPhoto from "./5-photo-reject"
 export default function handleProfileSignup(firstName, lastName, fileName) {
 	return Promise.allSettled([SignUpUser(firstName, lastName), uploadPhoto(fileName)])
-	.then((res) => console.log(res));
+	.then((res) => {
+		const result = [];
+		res.forEach((element) => {
+			if (element.status === 'fufilled') {result.push({status: element.status, value: element.value})
+			}
+			else { result.push({status: element.status, value: element.reason});
+			}
+			return result;
+		});
+	});
 }
