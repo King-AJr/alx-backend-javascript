05b4d074c616b057cfbd036df1be2d23e28848eb
King-AJr <talk2king.aj@gmail.com>
2023-07-28 18:46:10 +0000

    going through with all promises even if one rejects

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
-export default function SignupUser(firstName, lastName) {
+export default function SignUpUser(firstName, lastName) {
   return new Promise((resolve) => resolve({
     firstName, lastName
   }));
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,5 @@
+import SignUpUser from "./4-user-promise"
+import uploadPhoto from "./5-photo-reject"
+export default function handleProfileSignup(firstName, lastName, fileName) {
+	return Promise.allSettled([SignUpUser(firstName, lastName), uploadPhoto(fileName)]);
+}
