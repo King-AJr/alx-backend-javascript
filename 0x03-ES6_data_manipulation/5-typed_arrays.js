58e70cab965d904c3aa0ecca1beeff8146a2f596
King-AJr <talk2king.aj@gmail.com>
2023-08-01 10:55:25 +0000

    use array buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value ) {
-	if (position < 0 || position > length) {{
+	if (position < 0 || position > length) {
 		throw new Error("Position outside range");
+	}
 	const buffer = new ArrayBuffer(length);
-	const view = new Int8Array(buffer);
-	view[position] = value;
+	const view = new Int8Array(buffer, 0, length);
+	view.set([value], position);
 
-	return view;
+	return new DataView(buffer);
 }
