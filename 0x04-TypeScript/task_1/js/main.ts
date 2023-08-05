51ba573ec1144d223e286ac78f9203c4f182dfc1
King-AJr <talk2king.aj@gmail.com>
2023-08-05 14:55:02 +0100

    an interface named Directors that extends Teacher

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+interface Teacher {
+    readonly firstName: String,
+    readonly lastName: String,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: String,
+    [propName: string]: any
+}
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+
+interface Directors extends Teacher {
+    numberOfReports: number
+}
