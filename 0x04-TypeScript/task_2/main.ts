4919b52e17f3a4073d719fd9be7d49623621ff31
King-AJr <talk2king.aj@gmail.com>
2023-08-06 18:07:16 +0100

    String literal type named Subjects allowing a variable to have the value Math or History only

--- 0x04-TypeScript/task_2/main.ts
+++ 0x04-TypeScript/task_2/main.ts
@@ -58,3 +58,14 @@ function executeWork(employee: Director | Teacher) {
         employee.workTeacherTasks()
     }
 }
+
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else {
+        return 'Teaching History';
+    }
+}
