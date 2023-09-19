60412803df7e2240f89e3345131d17ce202b027d
King-AJr <talk2king.aj@gmail.com>
2023-09-19 17:14:07 +0100

    filter out empty lines and correct error message

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -7,18 +7,19 @@ function checkField(lines) {
   const CS = []; // Array to store students with 'CS' field
   const SWE = []; // Array to store students with 'SWE' field
   for (i = 0; i < lines.length; i += 1) {
-    // Split each line into an array using ',' as the delimiter
-    const line = lines[i].split(',');
-
-    // Get the last element of the line, which represents the field
-    const field = line[line.length - 1];
-    if (field === 'CS') {
-      // If the field is 'CS', add the first element (student) to the 'CS' array
-      CS.push(line[0]);
-    }
-    if (field === 'SWE') {
-      // If the field is 'SWE', add the first element (student) to the 'SWE' array
-      SWE.push(line[0]);
+    if (lines[i]) {
+      // Split each line into an array using ',' as the delimiter
+      const line = lines[i].split(',');
+      // Get the last element of the line, which represents the field
+      const field = line[line.length - 1];
+      if (field === 'CS') {
+        // If the field is 'CS', add the first element (student) to the 'CS' array
+        CS.push(line[0]);
+      }
+      if (field === 'SWE') {
+        // If the field is 'SWE', add the first element (student) to the 'SWE' array
+        SWE.push(line[0]);
+      }
     }
   }
   return [CS, SWE]; // Return an array containing both 'CS' and 'SWE' student arrays
@@ -26,31 +27,30 @@ function checkField(lines) {
 
 // Function to count and display the number of students in different fields from a given file path
 function countStudents(path) {
-  fs.readFileSync(path, 'utf8', (err, data) => { // Read the file asynchronously
-    if (err) {
-      console.error('Cannot load the database'); // Handle errors if the file cannot be read
-      return;
-    }
+  try {
+    fs.readFileSync(path, 'utf8', (err, data) => { // Read the file asynchronously
+      const lines = data.split('\n'); // Split the file content into an array of lines
+      const nonEmptyLines = lines.filter((line) => line.trim() !== ''); // Filter out empty lines
 
-    const lines = data.split('\n'); // Split the file content into an array of lines
-    const nonEmptyLines = lines.filter((line) => line.trim() !== ''); // Filter out empty lines
+      if (nonEmptyLines.length === 0) {
+        console.log('No student data found in the file.');
+        return;
+      }
 
-    if (nonEmptyLines.length === 0) {
-      console.log('No student data found in the file.');
-      return;
-    }
+      // Get the total number of non-empty lines (excluding headers)
+      const numberOfLines = nonEmptyLines.length;
 
-    // Get the total number of non-empty lines (excluding headers)
-    const numberOfLines = nonEmptyLines.length;
-
-    // Categorize non-empty lines into 'CS' and 'SWE' arrays
-    const students = checkField(nonEmptyLines);
-    const CSStudentList = students[0].join(', '); // Convert the 'CS' student array to a comma-separated string
-    const SWEStudentList = students[1].join(', '); // Convert the 'SWE' student array to a comma-separated string
-    console.log(`number of students: ${numberOfLines - 1}`); // Display the total number of students (excluding headers)
-    console.log(`Number of students in CS: ${students[0].length}. List: ${CSStudentList}`); // Display 'CS' student count and list
-    console.log(`Number of students in SWE: ${students[1].length}. List: ${SWEStudentList}`); // Display 'SWE' student count and list
-  });
+      // Categorize non-empty lines into 'CS' and 'SWE' arrays
+      const students = checkField(nonEmptyLines);
+      const CSStudentList = students[0].join(', '); // Convert the 'CS' student array to a comma-separated string
+      const SWEStudentList = students[1].join(', '); // Convert the 'SWE' student array to a comma-separated string
+      console.log(`number of students: ${numberOfLines - 1}`); // Display the total number of students (excluding headers)
+      console.log(`Number of students in CS: ${students[0].length}. List: ${CSStudentList}`); // Display 'CS' student count and list
+      console.log(`Number of students in SWE: ${students[1].length}. List: ${SWEStudentList}`); // Display 'SWE' student count and list
+    });
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
 }
 
 // Export the 'countStudents' function for use in other modules
