931f0a3d56d74a2a059c063b139ace871ebbb26e
King-AJr <talk2king.aj@gmail.com>
2023-09-19 15:39:25 +0100

    recieve users name from terminal and output message

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+function inputFx() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Welcome to Holberton School, what is your name?\n', (userInput) => {
+    console.log(`Your name is: ${userInput}`);
+    console.log('This important software is now closing');
+    rl.close();
+  });
+}
+
+inputFx();
