6e36d7bbe08ec9f06db56f50c00e6851ab22c92d
King-AJr <talk2king.aj@gmail.com>
2023-09-21 22:56:37 +0100

    test for correct output using varying values and that console is called once

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon')
+const {expect} = require('chai')
+const sendPaymentRequestToApi = require('./5-payment')
+
+
+describe('sendPaymentRequestToApi', () => {
+    it('', () => {
+        const consoleLogSpy = sinon.spy(console, 'log');
+
+        sendPaymentRequestToApi(100, 20)
+
+        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 120');
+        expect(consoleLogSpy.callCount).to.be.equal(1);
+        consoleLogSpy.restore()
+
+        sendPaymentRequestToApi(10, 10)
+        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 120');
+        expect(consoleLogSpy.callCount).to.be.equal(1);
+
+    })
+})
