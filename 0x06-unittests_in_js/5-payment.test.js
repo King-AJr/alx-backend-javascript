0eeb95312cc2954d7ee0858a4ef95f66fadb4ec0
King-AJr <talk2king.aj@gmail.com>
2023-09-21 23:07:58 +0100

    use beforeEach and afterEach to declare spy on a global scale

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -3,19 +3,30 @@ const {expect} = require('chai')
 const sendPaymentRequestToApi = require('./5-payment')
 
 
-describe('sendPaymentRequestToApi', () => {
-    it('test fn with varying values', () => {
-        const consoleLogSpy = sinon.spy(console, 'log');
+describe('sendPaymentRequestToApi: test fn with varying values', () => {
+    let consoleLogSpy;
 
+    beforeEach(() => {
+        if (!consoleLogSpy) {
+            consoleLogSpy = sinon.spy(console, 'log');
+        }
+    });
+
+    afterEach(() => {
+        consoleLogSpy.resetHistory();
+    });
+
+    it('test using 100 and 20', () => {
         sendPaymentRequestToApi(100, 20);
 
         expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 120');
         expect(consoleLogSpy.callCount).to.be.equal(1);
-        consoleLogSpy.restore();
+    })
 
+    it('test using 10 and 10', () => {
         sendPaymentRequestToApi(10, 10);
-        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 120');
-        expect(consoleLogSpy.callCount).to.be.equal(1);
 
+        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 20');
+        expect(consoleLogSpy.callCount).to.be.equal(1);
     })
 })
