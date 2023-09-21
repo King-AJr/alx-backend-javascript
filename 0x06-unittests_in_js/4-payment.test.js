ab80c076866d21c0bf4fe0d359562ad1a912dc5a
King-AJr <talk2king.aj@gmail.com>
2023-09-21 22:43:39 +0100

    testing with stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,14 +1,29 @@
 const sinon = require('sinon');
-const Utils = require('./utils')
-const {expect} = require('chai')
-const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const {expect} = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
     it('should use Utils.calculateNumber with the same arguments', () => {
-        const UtilsSpy = sinon.spy(Utils)
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+        calculateNumberStub.returns(10)
+
+        const consoleLogSpy = sinon.spy(console, 'log');
+
+        const result = calculateNumberStub('SUM', 100, 20);
+
 
         sendPaymentRequestToApi(100, 20);
-        expect(UtilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-        expect(UtilsSpy.calculateNumber.callCount).to.be.equal(1);
+        expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+        
+        expect(consoleLogSpy.callCount).to.be.equal(1);
+        expect(consoleLogSpy.firstCall.args[0]).to.equal('The total is: 10');
+        expect(result).to.equal(10);
+        // expect(calculateNumberStub.callCount).to.be.equal(1);
+        
+
+        calculateNumberStub.restore()
+        consoleLogSpy.restore()
+        
     })
 })
