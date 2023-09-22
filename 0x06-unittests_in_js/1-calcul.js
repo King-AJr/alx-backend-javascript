4b0924458e893a4e6602ba641f658935e9853ef6
King-AJr <talk2king.aj@gmail.com>
2023-09-22 15:00:32 +0100

    restructure function

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,11 @@
 const calculateNumber = (type, a, b) => {
-  let result = 0;
   if (type === 'SUM') {
-    result = Math.round(a) + Math.round(b);
+    return Math.round(a) + Math.round(b);
   } else if (type === 'SUBTRACT') {
-    result = Math.round(a) - Math.round(b);
+    return Math.round(a) - Math.round(b);
   } else if (type === 'DIVIDE') {
-    result = Math.round(a) / Math.round(b);
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
-  return result;
 };
 
 module.exports = calculateNumber;
