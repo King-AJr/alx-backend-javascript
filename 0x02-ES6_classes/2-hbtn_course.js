08b0f2346cb30336f2bd9a5c53bb298dab0c4112
King-AJr <talk2king.aj@gmail.com>
2023-11-09 00:42:43 +0100

    make corrects to tasks 2 and 6

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,3 @@
-function areAllStrings(arr) {
-  return arr.every((item) => typeof item === 'string');
-}
-
 export default class HolbertonCourse {
   constructor(name, length, students) {
     this.name = name;
@@ -13,32 +9,35 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(n) {
-    if (typeof n !== 'string') {
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = n;
+    this._name = value;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(n) {
-    if (typeof n !== 'number') {
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = n;
+    this._length = value;
   }
 
   get students() {
-    return this._length;
+    return this._students;
   }
 
-  set students(n) {
-    if (!areAllStrings(n)) {
+  set students(value) {
+    if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
-    this._students = n;
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,19 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(n) {
-    this._floors = n;
+  set floors(value) {
+    this._floors = value;
   }
 
-  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
